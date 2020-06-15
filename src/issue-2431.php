3817dc540ef2e8a7b93bb2ab4c91d80a056c230c
jrfnl <jrfnl@users.noreply.github.com>
2020-06-15 21:45:55 +0200

    Demo code & screenshots for issue 2431
    Ref: https://github.com/phpDocumentor/phpDocumentor/issues/2431

---
+++ src/issue-2431.php
@@ -0,0 +1,13 @@
+<?php
+
+class Issue2431 {
+
+    /**
+     * Check whether the `--ignore-annotations` option has been used.
+     *
+     * @since 1.0.0
+     *
+     * @return bool `true` if annotations should be ignored, `false` otherwise.
+     */
+    public static function ignoreAnnotations() {}
+}
