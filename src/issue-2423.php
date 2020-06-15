c3affad257c650e6b6e53da577d7f85a385a5370
jrfnl <jrfnl@users.noreply.github.com>
2020-06-15 03:48:24 +0200

    Demo code & screenshots for issue 2423
    Ref: https://github.com/phpDocumentor/phpDocumentor/issues/2423

---
+++ src/issue-2423.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Class summary.
+ *
+ * A description containing an inline {@see \Vendor\Package\Foo} tag to a class outside of the project.
+ *
+ * @see \Vendor\Package\Foo
+ */
+class Issue2423 {}
