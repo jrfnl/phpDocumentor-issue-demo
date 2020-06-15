b09df90b2e55d6062fd3477d4ac849ca18e42245
jrfnl <jrfnl@users.noreply.github.com>
2020-06-15 03:42:20 +0200

    Demo code for issue 2422
    Ref: https://github.com/phpDocumentor/phpDocumentor/issues/2422

---
+++ src/issue-2422.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Class summary
+ *
+ * A description containing an inline {@see \Issue2422} tag and another {@see \Issue2422::$property}.
+ *
+ * @see \Issue2422            Class
+ * @see \Issue2422::CONSTANT  Constant
+ * @see \Issue2422::$property Property
+ * @see \Issue2422::method    Method
+ */
+class Issue2422
+{
+    const CONSTANT = 'foo';
+
+    public $property;
+
+    public function method() {}
+}
