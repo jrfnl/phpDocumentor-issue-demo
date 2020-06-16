519d06401e1d99632ecbf7fcf21c86bc59a8640e
jrfnl <jrfnl@users.noreply.github.com>
2020-06-16 15:49:59 +0200

    Demo code & screenshots for issue 2445 - 3. *in* source dir
    Ref: https://github.com/phpDocumentor/phpDocumentor/issues/2445

--- src/issue-2445.php
+++ src/issue-2445.php
@@ -6,6 +6,6 @@
  * Typical usage:
  *
  * Example file `path/to/ConcreteClass.php`:
- * {@example "./usage-examples/ConcreteClassExample.php" Example test file.}
+ * {@example "ConcreteClassExample.php" Example test file.}
  */
 abstract class Issue2445 {}
