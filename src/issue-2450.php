5e86995304705053a1dd6c78c73209293c4cebb8
jrfnl <jrfnl@users.noreply.github.com>
2020-06-17 10:59:47 +0200

    Demo code & screenshots for issue 2450
    Ref: https://github.com/phpDocumentor/phpDocumentor/issues/2450

---
+++ src/issue-2450.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Summary.
+ *
+ * @link https://phpdoc.org
+ * @link https://www.php.net/language.oop5.paamayim-nekudotayim PHP Manual on OO forwarding calls
+ */
+class Issue2450 {}
